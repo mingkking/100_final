897c29ab02757182feac323b62049de989dae77a
HyeGyeong Cho <judyhg.cho@gmail.com>
2024-06-24 17:02:33 +0900

    헤경포크빼고

--- src/views/sample-page/SamplePage.js
+++ src/views/sample-page/SamplePage.js
@@ -9,7 +9,7 @@ const SamplePage = () => {
     <PageContainer title="Sample Page" description="this is Sample page">
 
       <DashboardCard title="Sample Page">
-        <Typography>이거슨샘플zzz</Typography>
+        <Typography>이거슨샘플ㅋ</Typography>
       </DashboardCard>
     </PageContainer>
   );
