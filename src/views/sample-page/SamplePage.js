30ab45aa5a467ef8570b962543490a477a111e32
WIN-02DQROLM1JH\ict03_016 <dlwogus588866@naver.com>
2024-06-25 16:25:13 +0900

    ㅁㄴㅇ

--- src/views/sample-page/SamplePage.js
+++ src/views/sample-page/SamplePage.js
@@ -17,37 +17,12 @@ const SamplePage = () => {
 
       mapRef.current = new window.kakao.maps.Map(container, options);
 
+      
+
       const zoomControl = new window.kakao.maps.ZoomControl();
       mapRef.current.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
 
-      // 주소-좌표 변환 객체를 생성합니다
-      var geocoder = new window.kakao.maps.services.Geocoder();
-
-      // 주소로 좌표를 검색합니다
-      geocoder.addressSearch('제주특별자치도 제주시 첨단로 242', function(result, status) {
-
-        // 정상적으로 검색이 완료됐으면 
-        if (status === window.kakao.maps.services.Status.OK) {
-
-            var coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
-
-            // 결과값으로 받은 위치를 마커로 표시합니다
-            var marker = new window.kakao.maps.Marker({
-                map: mapRef.current,
-                position: coords
-            });
-
-            // 인포윈도우로 장소에 대한 설명을 표시합니다
-            var infowindow = new window.kakao.maps.InfoWindow({
-                content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
-            });
-            infowindow.open(mapRef.current, marker);
-
-            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-            mapRef.current.setCenter(coords);
-        } 
-      }); 
-
+     
 
       function getInfo() {
         const center = mapRef.current.getCenter();
@@ -56,7 +31,6 @@ const SamplePage = () => {
         const bounds = mapRef.current.getBounds();
         const swLatLng = bounds.getSouthWest();
         const neLatLng = bounds.getNorthEast();
-        const boundsStr = bounds.toString();
 
         let message = '지도 중심좌표는 위도 ' + center.getLat() + ', <br>';
         message += '경도 ' + center.getLng() + ' 이고 <br>';
@@ -69,6 +43,8 @@ const SamplePage = () => {
       }
 
       getInfo();
+
+      
     }
   }, []);
 
