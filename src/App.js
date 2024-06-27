1a96146108ec42b42c4d5f1fdb16ab21c7abb240
WIN-02DQROLM1JH\ict03_016 <dlwogus588866@naver.com>
2024-06-27 14:25:08 +0900

    2024-06-27 14:24 재현수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { useRoutes } from 'react-router-dom';
-import Router from './routes/Router';
+import React from 'react';
 
-import { baselightTheme } from "./theme/DefaultColors";
+import { Box } from '@mui/material';
+
+import ResponsiveAppBar from './layout/Header';
+import Login from './views/Login';
+import MainPage from './views/MainPage';
+import Moglog from './views/Moglog';
+import Laboratory from './views/Laboratory';
+import Chatting from './views/Chatting';
+import Community from './views/Community';
+
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
 
 function App() {
-  const routing = useRoutes(Router);
-  const theme = baselightTheme;
-  return (
-    <ThemeProvider theme={theme}>
-
-      <CssBaseline />
-      {routing}
-
-    </ThemeProvider>
-  );
+    return (
+      <div>
+          <BrowserRouter>
+          <ResponsiveAppBar/>
+            <Routes>
+                <Route path='/Login' element={<Login/>}/>
+                <Route path='/MainPage' element={<MainPage/>}/>
+                <Route path='/Moglog' element={<Moglog/>}/>
+                <Route path='/Laboratory' element={<Laboratory/>}/>
+                <Route path='/Chatting' element={<Chatting/>}/>
+                <Route path='/Community' element={<Community/>}/>
+            </Routes>
+            </BrowserRouter>
+      </div>
+    )
 }
 
 export default App;
